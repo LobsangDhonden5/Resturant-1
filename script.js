9668c380698490bc054b8d199c3995459d8d7d8e
hethen92 <hethen.saini@gmail.com>
2016-10-07 14:16:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ if (hourNow > 18) {
 }
    
 document.write('<h3>' + greeting + '</h3>');
+
+var today = new Date();
+var hourNow = oday.getHours();
+var greeting;
+
+if (hourNow > 18) {
+  greeting = 'Good Evening!';
+} else if (hourNow > 12 {
+           greeting =' Good afternoon!';
+} else if (hourNow > 0) {
+  greeting = 'Good Morning!';
+} else {
+  greeting = ' Welcome!';
+}
+   
+document.write('<h3>' + greeting + '</h3>');
